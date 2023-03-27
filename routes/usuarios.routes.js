c2618559937e6b3dd5551cc3d52003d3cefef7cb
Santiago Cardona <santiagocardg@gmail.com>
2023-03-27 18:32:04 -0500

    centralizacion de la validacion del rol

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const Role = require('../models/role');
 
 const { validarCampos } = require('../middlewares/validar.campos');
+const { esRoleValido } = require('../helpers/db-validators');
 
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller');
+
 
 const router = Router();
 
@@ -17,12 +18,7 @@ router.post('/',[
     check('password','el password es obligatorio y mas de 6 letras').isLength({min: 6}),
     check('correo','El correo no es valido').isEmail(),
     //check('rol','No es un rol permitido').isIn(['Admin_Rol','User_Rol']),
-    check('rol').custom( async (rol = '')=>{
-        const existeRol = await Role.findOne({ rol });
-        if (!existeRol) {
-            throw new Error(`El rol ${rol} no esta registrado en la base de datos`)
-        }
-    }),
+    check('rol').custom( esRoleValido ),
     validarCampos
 ],usuariosPost);
 
