d590f8a94ef2cc348d82fcb664ab03156359246d
Santiago Cardona <santiagocardg@gmail.com>
2023-03-27 16:52:52 -0500

    validacion de campos

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -1,6 +1,8 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/usuario');
+
 
 const usuariosGet = (req,res)=>{
     const {q, nombre, apikey} = req.query;
@@ -14,10 +16,25 @@ const usuariosGet = (req,res)=>{
 
 const usuariosPost = async(req,res)=>{
 
-    const body = req.body;
 
-    const usuario = new Usuario(body);
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
 
+    //verificar si el correo existe
+
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        return res.status(400).json({
+            msg:'El correo ya esta registrado'
+        })
+    }
+
+    // encriptar la contraseña
+
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password,salt);
+
+    // guardar en bd
     await usuario.save();
 
     res.json({
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios.controller');
+const { validarCampos } = require('../middlewares/validar.campos');
 
 const router = Router();
 
@@ -7,7 +9,13 @@ router.get('/',usuariosGet);
 
 router.put('/:id',usuariosPut);
 
-router.post('/',usuariosPost);
+router.post('/',[
+    check('nombre','el nombre es obligatorio').notEmpty(),
+    check('password','el password es obligatorio y mas de 6 letras').isLength({min: 6}),
+    check('correo','El correo no es valido').isEmail(),
+    check('rol','No es un rol permitido').isIn(['Admin_Rol','User_Rol']),
+    validarCampos
+],usuariosPost);
 
 router.delete('/',usuariosDelete);
 
