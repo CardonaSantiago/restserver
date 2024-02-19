300d9870cf84480155faa0d05655b4e89c52ceb0
Santiago Cardona <santiagocardg@gmail.com>
2024-02-19 14:07:59 -0500

    migrando los archivos a helpers

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,46 +1,21 @@
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+
 
 const { response } = require("express");
+const { subirArchivo } = require("../helpers");
 
 
-const cargarArchivo = (req, res =  response)=>{
+const cargarArchivo = async(req, res =  response)=>{
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({msg: 'No archivos que subir'});
     return;
     }
 
-    const { archivo } = req.files;
+    const nombre = await subirArchivo(req.files);
 
-    const nombreCortado =  archivo.name.split('.');
-    const extension = nombreCortado[nombreCortado.length-1]
-
-
-    // validar extension
-
-    const extensionesPermitidas = ['png','jpg','jpeg','gif'];
-
-    if(!extensionesPermitidas.includes(extension)){
-        return res.status(400).json({
-            msg:`La extension ${ extension } no esta permitida, ${extensionesPermitidas}`
-        });
-    }
-
-
-
-    const nombreTemporal =  uuidv4()+'.'+extension;
-
-    
-    const uploadPath = path.join(__dirname, '../uploads/', nombreTemporal);
-
-    archivo.mv(uploadPath, function(err) {
-    if (err) {
-        return res.status(500).json({err});
-    }
-
-    res.json({msg: 'File uploaded to ' + uploadPath});
-    });
+    res.json({
+        msg: nombre
+    })
      
 }
 
---
+++ helpers/subir-archivo.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+
+const subirArchivo = (files, extensionesPermitidas = ['png','jpg','jpeg','gif'],carpeta = '')=>{
+
+    return new Promise((resolve, reject)=>{
+        const { archivo } = files;
+
+        const nombreCortado =  archivo.name.split('.');
+        const extension = nombreCortado[nombreCortado.length-1]
+
+
+        // validar extension
+
+        if(!extensionesPermitidas.includes(extension)){
+
+            return reject(`La extension ${ extension } no esta permitida, ${extensionesPermitidas}`)
+        }
+
+        const nombreTemporal =  uuidv4()+'.'+extension;
+
+        
+        const uploadPath = path.join(__dirname, '../uploads/',carpeta, nombreTemporal);
+
+        archivo.mv(uploadPath, function(err) {
+        if (err) {
+            reject(err);
+        }
+
+        resolve(nombreTemporal);
+        });    
+    })
+
+    
+}
+
+
+module.exports = {
+    subirArchivo
+}
